3b9105a37a78bdba0c02ec4f76402d963817f777
RangarajKaushikSundar <kaushik.rangaraj@gmail.com>
2017-03-05 18:25:49 -0700

    refactor(examples): Remove non useful information

--- Spring IOC/src/com/cg/loose/OutputWriter.java
+++
@@ -1,5 +0,0 @@
-package com.cg.loose;
-
-public interface OutputWriter {
-		public void generateOutput();
-}
--- Spring IOC/src/com/cg/welcome/Demo.java
+++
@@ -1,7 +0,0 @@
-package com.cg.welcome;
-
-public class Demo {
-		public Demo(){
-			System.out.println("Demo constructor");
-		} 
-}
